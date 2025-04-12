6d0fc87a92c8ff06cb91b24c9efb96a36937cfc5
vmkmanokumar <intern1@veyyil.com>
2025-04-12 13:10:54 +0530

    added for verscel delpoy ment

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from app import app
 from app.routes import api
+import os
 
 # Register the API blueprint
 app.register_blueprint(api)
 
-
-
-
+# Set the port from environment variable or default to 5000
+port = int(os.environ.get("PORT", 5000))
 
 @app.route("/")
 def index():
     return "Hello, World!"
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
